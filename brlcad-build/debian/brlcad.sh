ec7f8ae7888b16dd730acc78e919b905b401a5cf
Tom Browder <tom.browder@gmail.com>
2013-04-10 06:39:20 -0500

    update to upstream rev 55105

--- brlcad-build/debian/brlcad.sh
+++ brlcad-build/debian/brlcad.sh
@@ -1,16 +1,23 @@
-TPATH=/usr/brlcad/bin
-TMANPATH=/usr/brlcad/share/man
+BRLCAD_PATH=/usr/brlcad/bin
+BRLCAD_MANPATH=/usr/brlcad/share/man
 
-if [ -n "$PATH" ] ; then
-  PATH=${PATH}:${TPATH}:
-else
-  PATH=$TPATH
-fi
-if [ -n "$MANPATH" ] ; then
-  MANPATH=${MANPATH}:${TMANPATH}
-else
-  MANPATH=${TMANPATH}
+if [ -z "$PATH" ]
+then
+	export PATH=${BRLCAD_PATH}
+# check if $BRLCAD_PATH is already in $PATH
+elif echo $PATH | awk -F: -v j=$BRLCAD_PATH '{for(i=1;i<=NF;i++){if($i == j){exit 1}}}'
+then
+	export PATH=${PATH}:${BRLCAD_PATH}
 fi
 
-export PATH=$PATH:/usr/brlcad/bin
-export MANPATH=$MANPATH:/usr/brlcad/share/man
+# check if $BRLCAD_MANPATH is already in $MANPATH
+if echo $MANPATH | awk -F: -v j=$BRLCAD_MANPATH '{for(i=1;i<=NF;i++){if($i == j){exit 1}}}'
+then
+	# check if $MANPATH ends with a colon
+	if [ "${MANPATH#${MANPATH%?}}" == ":" ]
+	then
+		export MANPATH=${MANPATH}${BRLCAD_MANPATH}:
+	else
+		export MANPATH=${MANPATH}:${BRLCAD_MANPATH}
+	fi
+fi
